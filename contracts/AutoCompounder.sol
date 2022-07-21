9ea7dd03a3f8c71c4d9da68ab32e759fb4e77d63
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2022-07-21 14:46:14 -0300

    Simplify auto-compounder

--- contracts/AutoCompounder.sol
+++ contracts/AutoCompounder.sol
@@ -4,25 +4,28 @@ pragma solidity ^0.8.2;
 import "./IPeronio.sol";
 import "@openzeppelin/contracts_latest/access/Ownable.sol";
 
-contract AutoCompounder is Ownable {
-  IPeronio peronio;
 
-  constructor(address _peronio) {
-    peronio = IPeronio(_peronio);
-  }
+contract AutoCompounder is
+    Ownable
+{
+    IPeronio peronio;
 
-  uint256 public lastExecuted;
+    uint256 public lastExecuted;
 
-  function lastExec() internal view returns (bool) {
-    return ((block.timestamp - lastExecuted) > 43200); // 12 hours
-  }
+    constructor(
+        address _peronio
+    ) {
+        peronio = IPeronio(_peronio);
+    }
 
-  function autoCompound() public onlyOwner {
-    require(lastExec(), "autoCompound: Time not elapsed");
+    function autoCompound()
+        public
+        onlyOwner
+    {
+        require(12 * 60 * 60 < block.timestamp - lastExecuted, "autoCompound: Time not elapsed");
+        lastExecuted = block.timestamp;
 
-    peronio.claimRewards();
-    peronio.compoundRewards();
-
-    lastExecuted = block.timestamp;
-  }
+        peronio.claimRewards();
+        peronio.compoundRewards();
+    }
 }
