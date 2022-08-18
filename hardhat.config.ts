2fc7e3bc920730d6880d2187bb3661bbf231fd6a
Agustin Kassis <webmaster@masize.com>
2022-08-18 16:04:42 -0300

    Add 0.8.2 version for previous compilation

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -40,6 +40,15 @@ const config: HardhatUserConfig = {
                 },
             },
             {
+                version: "0.8.2",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+            {
                 version: "0.6.12",
                 settings: {
                     optimizer: {
