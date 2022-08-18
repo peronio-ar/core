2714b936d486e6295df51184f6ca09a255a57bcf
Agustin Kassis <webmaster@masize.com>
2022-08-18 18:50:32 -0300

    updated solidity version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -40,15 +40,6 @@ const config: HardhatUserConfig = {
                 },
             },
             {
-                version: "0.8.2",
-                settings: {
-                    optimizer: {
-                        enabled: true,
-                        runs: 200,
-                    },
-                },
-            },
-            {
                 version: "0.6.12",
                 settings: {
                     optimizer: {
