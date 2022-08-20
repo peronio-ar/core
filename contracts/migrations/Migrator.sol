34139a6726b25c042c4ffc944011a3f37568bca0
Agustin Kassis <webmaster@masize.com>
2022-08-20 13:28:54 -0300

    replaced contract wrapper for library

--- contracts/migrations/Migrator.sol
+++ contracts/migrations/Migrator.sol
@@ -1,13 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-import {IPeronioV1Wrapper} from "./old/IPeronioV1Wrapper.sol";
+import {PeronioV1Wrapper} from "./old/PeronioV1Wrapper.sol";
+import {IPeronioV1} from "./old/IPeronioV1.sol";
 import {IPeronio} from "../IPeronio.sol";
 
 // Interface
 import "./IMigrator.sol";
 
 contract Migrator is IMigrator {
+    using PeronioV1Wrapper for IPeronioV1;
+
     // Peronio V1 Wrapper Address
     address public immutable peronioV1Address;
 
@@ -22,7 +25,7 @@ contract Migrator is IMigrator {
 
     function quote(uint256 amount) external view override returns (uint256 usdc, uint256 pe) {
         // Peronio V1 Contract Wrapper
-        IPeronioV1Wrapper peronioV1 = IPeronioV1Wrapper(peronioV1Address);
+        IPeronioV1 peronioV1 = IPeronioV1(peronioV1Address);
         // Peronio V2 Contract
         IPeronio peronioV2 = IPeronio(peronioV2Address);
 
@@ -35,12 +38,12 @@ contract Migrator is IMigrator {
 
     function migrate(uint256 amount) external override returns (uint256 usdc, uint256 pe) {
         // Peronio V1 Contract Wrapper
-        IPeronioV1Wrapper peronioV1 = IPeronioV1Wrapper(peronioV1Address);
+        IPeronioV1 peronioV1 = IPeronioV1(peronioV1Address);
         // Peronio V2 Contract
         IPeronio peronioV2 = IPeronio(peronioV2Address);
 
         // Calculate USDC to be received by Peronio V1
-        usdc = peronioV1.withdraw(address(this), amount);
+        usdc = peronioV1.withdrawV2(address(this), amount);
 
         // Calculate PE to be minted by Peronio V2
         pe = peronioV2.mint(msg.sender, usdc, 1);
--- contracts/migrations/old/PeronioV1Wrapper.sol
+++ contracts/migrations/old/PeronioV1Wrapper.sol
@@ -1,83 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-import {IERC20} from "@openzeppelin/contracts_latest/token/ERC20/IERC20.sol";
-
-import {SafeERC20} from "@openzeppelin/contracts_latest/token/ERC20/utils/SafeERC20.sol";
 import {IPeronioV1} from "./IPeronioV1.sol";
 
-import {IUniswapV2Pair} from "../../uniswap/interfaces/IUniswapV2Pair.sol";
-import {IPeronioV1Wrapper} from "./IPeronioV1Wrapper.sol";
-
-// QiDao
-import {IFarm} from "../../qidao/IFarm.sol";
+import {IERC20} from "@openzeppelin/contracts_latest/token/ERC20/IERC20.sol";
 
 // Needed for Babylonian square-root & combined-multiplication-and-division
 import {mulDiv} from "../../Utils.sol";
 
-contract PeronioV1Wrapper is IPeronioV1Wrapper {
-    using SafeERC20 for IERC20;
-
-    // Peronio V1 Address
-    address public immutable override peronioAddress;
-
-    // USDC Token Address
-    address public immutable override usdcAddress;
-    // MAI Token Address
-    address public immutable override maiAddress;
-
-    // LP USDC/MAI Address from QuickSwap
-    address public immutable override lpAddress;
-
-    // QuickSwap Router
-    address public immutable override quickSwapRouterAddress;
-
-    // QiDao Farm
-    address public immutable override qiDaoFarmAddress;
-    // QI Token Address
-    address public immutable override qiAddress;
-    // QiDao Pool ID
-    uint256 public immutable override qiDaoPoolId;
-
-    /**
-     * Construct a new Peronio contract
-     *
-     * @param _peronioAddress  Peronio V1 Address
-     */
-    constructor(address _peronioAddress) {
-        // Peronio V1 Contract
-        IPeronioV1 peronioContract = IPeronioV1(_peronioAddress);
-
-        // Peronio V1 Address
-        peronioAddress = _peronioAddress;
-
-        // Stable coins
-        usdcAddress = peronioContract.USDC_ADDRESS();
-        maiAddress = peronioContract.MAI_ADDRESS();
-
-        // LP USDC/MAI Address
-        lpAddress = peronioContract.LP_ADDRESS();
-
-        // Router
-        quickSwapRouterAddress = peronioContract.QUICKSWAP_ROUTER_ADDRESS();
-
-        // QiDao
-        qiDaoFarmAddress = peronioContract.QIDAO_FARM_ADDRESS();
-        qiAddress = peronioContract.QI_ADDRESS();
-        qiDaoPoolId = peronioContract.QIDAO_POOL_ID();
-    }
-
+library PeronioV1Wrapper {
     /**
      * Retrieve the expected number of USDC tokens corresponding to the given number of PE tokens for withdrawal.
      *
+     * @param peronioContract  Peronio contract interface
      * @param pe  Number of PE tokens to quote for
      * @return usdc  Number of USDC tokens quoted for the given number of PE tokens
      */
-    function quoteOut(uint256 pe) external view override returns (uint256 usdc) {
-        uint256 totalSupply = _totalSupply(); // save gas
+    function quoteOut(IPeronioV1 peronioContract, uint256 pe) external view returns (uint256 usdc) {
+        uint256 totalSupply = IERC20(address(peronioContract)).totalSupply(); // save gas
 
-        (uint256 usdcReserves, uint256 maiReserves) = _getLpReserves();
-        (uint256 stakedUsdc, uint256 stakedMai) = _stakedTokens();
+        (uint256 usdcReserves, uint256 maiReserves) = peronioContract.getLpReserves();
+        (uint256 stakedUsdc, uint256 stakedMai) = _stakedTokens(peronioContract);
 
         uint256 usdcAmount = mulDiv(pe, stakedUsdc, totalSupply);
         uint256 maiAmount = mulDiv(pe, stakedMai, totalSupply);
@@ -89,60 +32,41 @@ contract PeronioV1Wrapper is IPeronioV1Wrapper {
     /**
      * Extract the given number of PE tokens as USDC tokens
      *
+     * @param peronioContract  Peronio contract interface
      * @param to  Address to deposit extracted USDC tokens into
      * @param peAmount  Number of PE tokens to withdraw
      * @return usdcTotal  Number of USDC tokens extracted
      * @custom:emit  Withdrawal
      */
-    function withdraw(address to, uint256 peAmount) external override returns (uint256 usdcTotal) {
+    function withdrawV2(
+        IPeronioV1 peronioContract,
+        address to,
+        uint256 peAmount
+    ) external returns (uint256 usdcTotal) {
+        address usdcAddress = peronioContract.USDC_ADDRESS();
         uint256 oldUsdcBalance = IERC20(usdcAddress).balanceOf(to);
-        IPeronioV1(peronioAddress).withdraw(to, peAmount);
 
-        (bool success, ) = peronioAddress.delegatecall(abi.encodeWithSignature("withdraw(address,uint256)", to, peAmount));
+        // peronioContract.withdraw(to, peAmount);
+
+        (bool success, ) = address(peronioContract).delegatecall(abi.encodeWithSignature("withdraw(address,uint256)", to, peAmount));
         require(success, "Error delegating call");
         usdcTotal = IERC20(usdcAddress).balanceOf(to) - oldUsdcBalance;
     }
 
     /**
-     * Return the USDC and MAI token reserves present in QuickSwap
-     *
-     * @return usdcReserves  Number of USDC tokens in reserve
-     * @return maiReserves  Number of MAI tokens in reserve
-     */
-    function _getLpReserves() private view returns (uint256 usdcReserves, uint256 maiReserves) {
-        (uint112 reserve0, uint112 reserve1, ) = IUniswapV2Pair(lpAddress).getReserves();
-        (usdcReserves, maiReserves) = usdcAddress < maiAddress ? (uint256(reserve0), uint256(reserve1)) : (uint256(reserve1), uint256(reserve0));
-    }
-
-    /**
-     * @dev Returns the amount of tokens in existence.
-     */
-    function _totalSupply() private view returns (uint256) {
-        return IERC20(peronioAddress).totalSupply();
-    }
-
-    /**
      * Return the number of USDC and MAI tokens on stake at QiDao's Farm
-     *
+     * @param peronioContract  Peronio contract interface
      * @return usdcAmount  Number of USDC tokens on stake
      * @return maiAmount  Number of MAI tokens on stake
      */
-    function _stakedTokens() private view returns (uint256 usdcAmount, uint256 maiAmount) {
-        uint256 lpAmount = _stakedBalance();
+    function _stakedTokens(IPeronioV1 peronioContract) private view returns (uint256 usdcAmount, uint256 maiAmount) {
+        uint256 lpAmount = peronioContract.stakedBalance();
+        address lpAddress = peronioContract.LP_ADDRESS();
         uint256 lpTotalSupply = IERC20(lpAddress).totalSupply();
 
-        (uint256 usdcReserves, uint256 maiReserves) = _getLpReserves();
+        (uint256 usdcReserves, uint256 maiReserves) = peronioContract.getLpReserves();
 
         usdcAmount = mulDiv(lpAmount, usdcReserves, lpTotalSupply);
         maiAmount = mulDiv(lpAmount, maiReserves, lpTotalSupply);
-    }
-
-    /**
-     * Return the number of LP USDC/MAI tokens on stake at QiDao's Farm
-     *
-     * @return lpAmount  Number of LP USDC/MAI token on stake
-     */
-    function _stakedBalance() private view returns (uint256 lpAmount) {
-        lpAmount = IFarm(qiDaoFarmAddress).deposited(qiDaoPoolId, peronioAddress);
     }
 }
