7ff63825e9184c98c7dfd8557a6fe768e1a1531f
Agustin Kassis <webmaster@masize.com>
2022-08-20 13:30:41 -0300

    removed wrapper deploy

--- deploy/03_peronio_migrator.ts
+++ deploy/03_peronio_migrator.ts
@@ -1,5 +1,5 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { DeployFunction, DeployResult } from "hardhat-deploy/types";
+import { DeployFunction } from "hardhat-deploy/types";
 
 const deployMigrator: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     console.info("Deploying Peronio V1 Wrapper");
@@ -8,18 +8,11 @@ const deployMigrator: DeployFunction = async (hre: HardhatRuntimeEnvironment) =>
     const peronioV1Address = process.env.PERONIO_V1_ADDRESS;
     const peronioV2Address = (await hre.deployments.get("Peronio")).address;
 
-    const peronioV1Wrapper: DeployResult = await hre.deployments.deploy("PeronioV1Wrapper", {
-        contract: "PeronioV1Wrapper",
-        from: deployer,
-        log: true,
-        args: [peronioV1Address],
-    });
-
     await hre.deployments.deploy("Migrator", {
         contract: "Migrator",
         from: deployer,
         log: true,
-        args: [peronioV1Wrapper.address, peronioV2Address],
+        args: [peronioV1Address, peronioV2Address],
     });
 };
 
