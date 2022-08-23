8a9912b76caee7fe20b2e9c4aa019a3c858c5e02
Agustin Kassis <webmaster@masize.com>
2022-08-23 17:09:11 -0300

    added transfer and approve to migrator

--- contracts/migrations/Migrator.sol
+++ contracts/migrations/Migrator.sol
@@ -5,22 +5,37 @@ import {PeronioV1Wrapper} from "./old/PeronioV1Wrapper.sol";
 import {IPeronioV1} from "./old/IPeronioV1.sol";
 import {IPeronio} from "../IPeronio.sol";
 
+import {IERC20} from "@openzeppelin/contracts_latest/token/ERC20/IERC20.sol";
+
 // Interface
 import "./IMigrator.sol";
 
 contract Migrator is IMigrator {
     using PeronioV1Wrapper for IPeronioV1;
 
-    // Peronio V1 Wrapper Address
+    // Peronio V1 Address
     address public immutable peronioV1Address;
 
-    // Peronio V2 Wrapper Address
+    // Peronio V2 Address
     address public immutable peronioV2Address;
 
-    constructor(address _peronioV1Address, address _peronioV2Address) {
+    // USDC Address
+    address public immutable usdcAddress;
+
+    constructor(
+        address _peronioV1Address,
+        address _peronioV2Address,
+        address _usdcAddress
+    ) {
         // Peronio Addresses
         peronioV1Address = _peronioV1Address;
         peronioV2Address = _peronioV2Address;
+
+        // USDC Address
+        usdcAddress = _usdcAddress;
+
+        // Unlimited USDC Approve to Peronio V2 contract
+        IERC20(_usdcAddress).approve(_peronioV2Address, type(uint256).max);
     }
 
     function quote(uint256 amount) external view override returns (uint256 usdc, uint256 pe) {
@@ -42,10 +57,16 @@ contract Migrator is IMigrator {
         // Peronio V2 Contract
         IPeronio peronioV2 = IPeronio(peronioV2Address);
 
+        // Transfer PE V1 to this contract
+        IERC20(peronioV1Address).transferFrom(msg.sender, address(this), amount);
+
         // Calculate USDC to be received by Peronio V1
         usdc = peronioV1.withdrawV2(address(this), amount);
 
         // Calculate PE to be minted by Peronio V2
         pe = peronioV2.mint(msg.sender, usdc, 1);
+
+        // Emit Migration event
+        emit Migration(block.timestamp, amount, usdc, pe);
     }
 }
