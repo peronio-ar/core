e59e87482eb384962603ddc7b5951f883bf02cc3
Agustin Kassis <webmaster@masize.com>
2022-08-23 17:08:40 -0300

    added events on migrator

--- contracts/migrations/IMigrator.sol
+++ contracts/migrations/IMigrator.sol
@@ -2,6 +2,18 @@
 pragma solidity ^0.8.16;
 
 interface IMigrator {
+    // --- Events ---------------------------------------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Emitted upon migration
+     *
+     * @param timestamp  The address initializing the contract
+     * @param oldPe  The number of old PE tokens withdraw
+     * @param usdc  The number of USDC tokens converted
+     * @param newPe  The number of new PE tokens migrated
+     */
+    event Migration(uint256 timestamp, uint256 oldPe, uint256 usdc, uint256 newPe);
+
     // Peronio Addresses
     function peronioV1Address() external view returns (address);
 
