f930c6843dc69e35840b987aa65e9a0ea05e1cf8
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2022-10-13 18:34:54 -0300

    Reorder imports

--- contracts/Peronio.sol
+++ contracts/Peronio.sol
@@ -2,14 +2,15 @@
 pragma solidity ^0.8.17;
 
 // OpenZeppelin imports
-import {AccessControl} from "@openzeppelin/contracts_latest/access/AccessControl.sol";
-import {ReentrancyGuard} from "@openzeppelin/contracts_latest/security/ReentrancyGuard.sol";
-import {ERC20} from "@openzeppelin/contracts_latest/token/ERC20/ERC20.sol";
 import {IERC20} from "@openzeppelin/contracts_latest/token/ERC20/IERC20.sol";
+import {ERC20} from "@openzeppelin/contracts_latest/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts_latest/token/ERC20/extensions/draft-ERC20Permit.sol";
 import {ERC20Burnable} from "@openzeppelin/contracts_latest/token/ERC20/extensions/ERC20Burnable.sol";
+
 import {SafeERC20} from "@openzeppelin/contracts_latest/token/ERC20/utils/SafeERC20.sol";
 
+import {AccessControl} from "@openzeppelin/contracts_latest/access/AccessControl.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts_latest/security/ReentrancyGuard.sol";
 import {Multicall} from "@openzeppelin/contracts_latest/utils/Multicall.sol";
 
 // QiDao
